5ad2fb96df605f1eee71d41394cf384b8cd30829
William Reima <william.reima@gmail.com>
2025-03-03 14:31:14 +0200

    added env

--- backend/cmd/main.go
+++ backend/cmd/main.go
@@ -1,16 +1,31 @@
 package main
 
 import (
+	"fmt"
 	"log"
+	"os"
 
 	"github.com/Missing-Link-harkat/mqtt-logger/internal/api"
 	"github.com/Missing-Link-harkat/mqtt-logger/internal/db"
 	"github.com/Missing-Link-harkat/mqtt-logger/internal/mqtt"
+	"github.com/joho/godotenv"
 )
 
 func main() {
 
-	dsn := "host=localhost user=postgres password=example dbname=example_db port=5432 sslmode=disable"
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatalf("Error loading .env file")
+	}
+
+	dsn := fmt.Sprintf("host=%s user=%s password=%s dbname=%s port=%s sslmode=%s",
+		getEnv("DB_HOST"),
+		getEnv("DB_USER"),
+		getEnv("DB_PASSWORD"),
+		getEnv("DB_NAME"),
+		getEnv("DB_PORT"),
+		getEnv("DB_SSLMODE"),
+	)
 	dbConn, err := db.ConnectDB(dsn)
 	if err != nil {
 		log.Fatalf("Error connecting to database: %v", err)
@@ -20,4 +35,12 @@ func main() {
 
 	r := api.SetupRouter()
 	r.Run(":8080")
+}
+
+func getEnv(key string) string {
+	value := os.Getenv(key)
+	if value == "" {
+		log.Fatalf("Missing required env variable: %s", key)
+	}
+	return value
 }
