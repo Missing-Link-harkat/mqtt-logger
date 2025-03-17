fb31efe394df9c7a07034d04d650aee32724eb48
William Reima <william.reima@gmail.com>
2025-03-17 15:38:43 +0200

    added support for mqtt username auth and SSL/TLS certificate

--- backend/cmd/main.go
+++ backend/cmd/main.go
@@ -3,45 +3,32 @@ package main
 import (
 	"fmt"
 	"log"
-	"os"
 
 	"github.com/Missing-Link-harkat/mqtt-logger/internal/api"
 	"github.com/Missing-Link-harkat/mqtt-logger/internal/db"
 	"github.com/Missing-Link-harkat/mqtt-logger/internal/mqtt"
-	"github.com/joho/godotenv"
+	"github.com/Missing-Link-harkat/mqtt-logger/internal/utils"
 )
 
 func main() {
 
-	err := godotenv.Load()
-	if err != nil {
-		log.Fatalf("Error loading .env file")
-	}
+	utils.LoadEnvVars()
 
 	dsn := fmt.Sprintf("host=%s user=%s password=%s dbname=%s port=%s sslmode=%s",
-		getEnv("DB_HOST"),
-		getEnv("DB_USER"),
-		getEnv("DB_PASSWORD"),
-		getEnv("DB_NAME"),
-		getEnv("DB_PORT"),
-		getEnv("DB_SSLMODE"),
+		utils.GetEnv("DB_HOST"),
+		utils.GetEnv("DB_USER"),
+		utils.GetEnv("DB_PASSWORD"),
+		utils.GetEnv("DB_NAME"),
+		utils.GetEnv("DB_PORT"),
+		utils.GetEnv("DB_SSLMODE"),
 	)
 	dbConn, err := db.ConnectDB(dsn)
 	if err != nil {
 		log.Fatalf("Error connecting to database: %v", err)
 	}
 
-	mqttBroker := fmt.Sprintf("tcp://%s:%s", getEnv("MQTT_HOST"), getEnv("MQTT_PORT"))
-	go mqtt.InitMQTT(mqttBroker, dbConn)
+	go mqtt.InitMQTT(dbConn)
 
 	r := api.SetupRouter()
 	r.Run(":8080")
-}
-
-func getEnv(key string) string {
-	value := os.Getenv(key)
-	if value == "" {
-		log.Fatalf("Missing required env variable: %s", key)
-	}
-	return value
 }
--- backend/internal/mqtt/client.go
+++ backend/internal/mqtt/client.go
@@ -1,25 +1,58 @@
 package mqtt
 
 import (
+	"crypto/tls"
+	"crypto/x509"
 	"fmt"
 	"log"
+	"os"
 	"time"
 
 	"github.com/Missing-Link-harkat/mqtt-logger/internal/db"
+	"github.com/Missing-Link-harkat/mqtt-logger/internal/utils"
 	mqtt "github.com/eclipse/paho.mqtt.golang"
 	"gorm.io/gorm"
 )
 
 var mqttClient mqtt.Client
 
-func InitMQTT(broker string, dbConn *gorm.DB) {
+func InitMQTT(dbConn *gorm.DB) {
+
+	mqttBroker := fmt.Sprintf("tcp://%s:%s", utils.GetEnv("MQTT_HOST"), utils.GetEnv("MQTT_PORT"))
+
+	mqttUsername := utils.GetEnv("MQTT_USERNAME")
+	mqttPassword := utils.GetEnv("MQTT_PASSWORD")
+	mqttCACertPath := utils.GetEnv("MQTT_CA_CERT_PATH")
+
 	opts := mqtt.NewClientOptions()
-	opts.AddBroker(broker)
+	opts.AddBroker(mqttBroker)
 	opts.SetClientID("go_mqtt_client")
 
 	opts.SetAutoReconnect(true)
 	opts.SetConnectRetry(true)
 	opts.SetConnectRetryInterval(3 * time.Second)
+	// Auth
+	opts.SetUsername(mqttUsername)
+	opts.SetPassword(mqttPassword)
+
+	// SSL/TLS config
+	tlsConfig := &tls.Config{
+		InsecureSkipVerify: false,
+	}
+
+	if mqttCACertPath != "" {
+		caCert, err := os.ReadFile(mqttCACertPath)
+		if err != nil {
+			log.Fatalf("Failed to read CA certificate: %v", err)
+		}
+		certPool := x509.NewCertPool()
+		if !certPool.AppendCertsFromPEM(caCert) {
+			log.Fatalf("Failed to append CA certificate to CertPool")
+		}
+		tlsConfig.RootCAs = certPool
+	}
+
+	opts.SetTLSConfig(tlsConfig)
 
 	opts.SetConnectionLostHandler(func(client mqtt.Client, err error) {
 		log.Printf("MQTT connection lost: %v", err)
